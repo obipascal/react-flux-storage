826a0777f158a2746d796239cdca4ba20d6433dd
Obi Pascal <pascalobi83@gmail.com>
2022-11-14 12:33:59 +0100

    updated

--- lib/flus/actions/flusActions.d.ts
+++ lib/flus/actions/flusActions.d.ts
@@ -1,8 +1,8 @@
 export declare const FlusActions: {
-    "flusapp/stores/create-dynamic-store": (state: import("../../types/types").FluxState, payload: import("../../types/types").FluxPayload) => {
+    "flux_app/stores/create-dynamic-store": (state: import("../../types/types").FluxState, payload: import("../../types/types").FluxPayload) => {
         [x: string]: any;
     };
-    "flusapp/events/emit-dynamic-event": (state: import("../../types/types").FluxState, payload: import("../../types/types").FluxPayload) => {
+    "flux_app/events/emit-dynamic-event": (state: import("../../types/types").FluxState, payload: import("../../types/types").FluxPayload) => {
         [x: string]: any;
     };
 };
